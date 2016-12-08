c05265e96c42926398e933aa0542e7504ccc2045
sitr <sigbjorn@trageton.net>
2016-12-08 14:34:32 +0100

    Added weather symbols

--- MMM-YrNow.js
+++ MMM-YrNow.js
@@ -45,36 +45,36 @@ Module.register('MMM-YrNow', {
         wrapper.className = 'light medium bright';
 
         //Add fake data
-        //this.list.points[5].precipitation.intensity = 0.5;
+        this.list.points[0].precipitation.intensity = 0.3;
 
         var precipitationStart = this.list.points.filter((item) => item.precipitation.intensity > 0)[0];
         var precipitationStop = this.list.points.filter((item) => item.precipitation.intensity === 0)[0];
-        var precipitationIntencityNow = this.list.points[0].precipitation.intensity;
-        
-        var nowCast = this.translate('no_precipitation');
-
+        var nowCast = this.translate('no_precip_next_90');
+        var precipSymbol = '';
         if(precipitationStart != null) {
             var precipitationStartsIn = Math.abs(new Date() - new Date(precipitationStart.time)) / (1000 * 60);
-            nowCast = printf(this.translate("precipitation_in"), precipitationStartsIn.toFixed(0));
-        }
-
-        if(precipitationStop != null) {
             var precipitationStopsIn = Math.abs(new Date() - new Date(precipitationStop.time)) / (1000 * 60);
-            nowCast = printf(this.translate("precipitation_ends"), precipitationStopsIn.toFixed(0));
+            if(this.list.points[0].precipitation.intensity === 0)
+                nowCast = printf(this.translate("no_precip_now_but"), precipitationStartsIn.toFixed(0));
+            else {
+                nowCast = printf(this.translate("precip_now_but"), precipitationStartsIn.toFixed(0));
+                precipSymbol = this.getSymbol(this.list.points[0].precipitation.intensity);
+            }
         }
-        if(precipitationIntencityNow > 0) {
-            var precipitationSymbol = '';
-            if(precipitationIntencityNow <= 0.1)
-                precipitationSymbol = '46.png';
-            else if(precipitationIntencityNow <= 0.4)
-                precipitationSymbol = '09.png';
-            else
-                precipitationSymbol = '10.png';
-            wrapper.innerHTML = printf("<img src='%s' />", precipitationSymbol);
-        }
-        wrapper.innerHTML += printf('<p>%s</p>', nowCast);
+        wrapper.innerHTML = printf('<p>%s<br/>%s</p>', precipSymbol, nowCast);
     	return wrapper;
 	},
+
+    getSymbol: function(intensity) {
+        var precipitationSymbol = '';
+        if(intensity <= 0.1)
+            precipitationSymbol = "<img src='" + this.file("/images/46.png") + "' />";
+        else if(intensity <= 0.4)
+            precipitationSymbol = "<img src='" + this.file("/images/09.png") + "' />";
+        else
+            precipitationSymbol = "<img src='" + this.file("/images/10.png") + "' />";
+        return precipitationSymbol
+    },
 
 	processJSON: function(obj) {
 		this.list = obj;
